857f740ca11f7a63737bb390c16c007b74895fb6
JISHUHALDAR <104211124+JISHUHALDAR@users.noreply.github.com>
2022-06-28 21:11:26 +0530

    Add files via upload

---
+++ 7qs.js
@@ -0,0 +1,12 @@
+// Join Strings
+// You are given two strings 
+// S
+//  and 
+// P
+// , and your task is to concatenate them and return the concatenated string
+
+ const joinStrings = (S, P) => 
+ {
+     return S.concat(P)
+ };
+ 
